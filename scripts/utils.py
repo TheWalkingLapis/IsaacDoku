8309039cc01385c6ef4f589bab3541e8aec6ac8a
TheWalkingLapis <max.kiebist@gmx.de>
2026-04-24 11:46:05 +0200

    feat: Add item selection (TODO: replace with custom dropdown for images etc.)

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import os
 from scripts import (
     dataCsvPathItemsFile,
@@ -21,6 +22,25 @@ def get_all_item_ids() -> list[int]:
         ITEMS_DF_CACHE = pd.read_csv(dataCsvPathItemsFile)
     
     return list(ITEMS_DF_CACHE["ID"])
+
+def get_all_items(returnCatgeories=[]) -> list[dict]:
+    """
+    empty returnCatgeories means return all
+    """
+    if not os.path.exists(dataCsvPathItemsFile):
+        print("ERROR: Item csv not present!")
+        return []
+    
+    global ITEMS_DF_CACHE
+    if ITEMS_DF_CACHE is None:
+        ITEMS_DF_CACHE = pd.read_csv(dataCsvPathItemsFile)
+    
+    # TODO remove by maing sure Nan is not exported
+    clearNanDf = ITEMS_DF_CACHE.replace({np.nan: None})
+
+    if len(returnCatgeories) == 0:
+        return clearNanDf.to_dict("records")
+    return clearNanDf[returnCatgeories].to_dict("records")
 
 def is_item_in_categories(itemID:int, categories: list[str]) -> bool:
     """
